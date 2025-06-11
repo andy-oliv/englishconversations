f56691d03c52bec35b429e73cadeabb4ca117ebe
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-06-11 14:49:05 -0300

    chore: installed and configured swagger

--- ec_backend/src/main.ts
+++ ec_backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -12,6 +13,36 @@ async function bootstrap() {
     }),
   );
   app.useLogger(app.get(Logger));
+
+  const config = new DocumentBuilder()
+    .setTitle('English Conversations API')
+    .setDescription(
+      "REST API for managing English Conversations' data and processes.",
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  const swaggerOptions = {
+    customSiteTitle: 'English Conversations API Docs',
+    customfavIcon: '/logo.ico',
+    customCss: `
+    .swagger-ui .topbar { background-color: #000000; }
+    #logo_small_svg__SW_TM-logo-on-dark { display: none; }
+    .swagger-ui .topbar .topbar-wrapper::before {
+      content: '';
+      background-image: url('/logo.png'); 
+      background-repeat: no-repeat;
+      background-size: contain;
+      height: 80px; 
+      width: 170px;
+      display: inline-block;
+      margin-right: 10px;
+    }
+  `,
+  };
+
+  SwaggerModule.setup('docs', app, documentFactory, swaggerOptions);
 
   await app.listen(3000);
 }

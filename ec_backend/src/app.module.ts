e3ad733b583925f5552ceb6eb5040e530342d133
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-06-11 14:24:14 -0300

    chore: installed and configured serve static for swagger customization

--- ec_backend/src/app.module.ts
+++ ec_backend/src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src', 'assets', 'images'),
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

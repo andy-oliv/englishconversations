615f2881f8bc543648bdc4cf68ff0aba560ef023
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-06-11 14:21:11 -0300

    chore: configured environment variable reading

--- ec_backend/src/app.module.ts
+++ ec_backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

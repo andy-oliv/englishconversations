aed7776583711fd826cc23fc8c2031fdf8693091
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-06-11 14:36:37 -0300

    chore: installed and configured Pino, connected to Better Stack

--- ec_backend/src/app.module.ts
+++ ec_backend/src/app.module.ts
@@ -6,12 +6,37 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'src', 'assets', 'images'),
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'debug',
+        transport: {
+          targets: [
+            {
+              target: 'pino-pretty',
+              level: 'debug',
+              options: { colorize: true },
+            },
+            {
+              target: '@logtail/pino',
+              level: 'debug',
+              options: {
+                sourceToken: process.env.BETTERSTACK_TOKEN,
+                options: {
+                  endpoint: `https://${process.env.BETTERSTACK_CONNECTION}`,
+                },
+              },
+            },
+          ],
+        },
+      },
     }),
     PrismaModule,
   ],

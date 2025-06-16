d9c650432d5aae0ed38f5bd8fc83648b847bea59
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-06-16 13:08:25 -0300

    chore(answeredExercise): updated dto with swagger documentation

--- ec_backend/src/answered-exercise/dto/fetchAnswerByQuery.dto.ts
+++ ec_backend/src/answered-exercise/dto/fetchAnswerByQuery.dto.ts
@@ -1,7 +1,14 @@
 import { IsInt, IsOptional, IsUUID } from 'class-validator';
 import validationMessages_EN from '../../helper/messages/validationMessages.en';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class fetchAnswerByQuery {
+  @ApiProperty({
+    title: 'StudentID',
+    required: false,
+    type: 'string',
+    example: 'f0aaa9ad-ee8e-42f0-a823-c90f2d255f5a',
+  })
   @IsOptional()
   @IsUUID('all', {
     message:
@@ -9,6 +16,12 @@ export default class fetchAnswerByQuery {
   })
   studentId?: string;
 
+  @ApiProperty({
+    title: 'ExerciseID',
+    required: false,
+    type: 'number',
+    example: 4,
+  })
   @IsOptional()
   @IsInt({
     message:
@@ -16,6 +29,12 @@ export default class fetchAnswerByQuery {
   })
   exerciseId?: number;
 
+  @ApiProperty({
+    title: 'QuizId',
+    required: false,
+    type: 'string',
+    example: 'f0aaa9ad-ee8e-42f0-a823-c90f2d255f5a',
+  })
   @IsOptional()
   @IsUUID('all', {
     message: validationMessages_EN.answeredExercise.saveAnswerDTO.quizId.isUUID,

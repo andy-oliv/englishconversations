139301f3d5422c2eaabbe6b8a11fcca19ed1d307
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-06-16 13:07:06 -0300

    chore(prisma): updated answeredQuiz table

---
+++ ec_backend/prisma/migrations/20250616155708_added_retry_into_answered_quiz_table/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `resetPasswordExpires` on the `user` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `lastLogin` on the `user` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `completedAt` on the `userunit` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `user` MODIFY `resetPasswordExpires` DATETIME NULL,
+    MODIFY `lastLogin` DATETIME NULL;
+
+-- AlterTable
+ALTER TABLE `userunit` MODIFY `completedAt` DATETIME NULL;

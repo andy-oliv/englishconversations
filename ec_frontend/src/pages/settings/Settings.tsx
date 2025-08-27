c7e52d4a391b9d64f2c8b123247accf54003f13c
Andrew Pacheco <andrew.pacheco@outlook.com>
2025-08-27 17:41:39 -0300

    feat(settings): added settings page

---
+++ ec_frontend/src/pages/settings/Settings.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+import styles from "./styles/Settings.module.scss";
+
+export default function Settings(): ReactElement {
+  return (
+    <>
+      <h1 className={styles.title}>SETTINGS</h1>
+    </>
+  );
+}
